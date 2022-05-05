ec3f0494c80eefcfb9cd742fabaf4678329cd143
isaniovitor <isaniovitot@gmail.com>
2022-05-05 11:28:52 -0300

    project config

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '~',
+          rootPathSuffix: 'src',
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
